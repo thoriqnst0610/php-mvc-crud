ed18d9913c91a761332ed467b05f3c013b28bc69
Thoriq <155645840+thoriqnst0610@users.noreply.github.com>
2024-08-01 10:08:25 +0700

    Create anggota.php

---
+++ app/domain/anggota.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace perpustakaan\domain;
+
+class anggota{
+    
+    public ?array $semua = [];
+    public ?string $id_anggota = null;
+    public ?string $nama = null;
+    public ?string $alamat = null;
+    public ?string $nik = null;
+    
+}
