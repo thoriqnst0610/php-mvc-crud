501f9008d2bf27e3705ce4a7ea4c2e93aa965d71
Thoriq <155645840+thoriqnst0610@users.noreply.github.com>
2024-08-01 10:08:48 +0700

    Add files via upload

---
+++ app/domain/Session.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace perpustakaan\Domain;
+
+class Session
+{
+    public string $id;
+    public string $userId;
+    public string $username;
+}
---
+++ app/domain/User.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace perpustakaan\Domain;
+
+class User
+{
+
+    public string $username;
+    public string $id;
+    public string $name;
+    public string $password;
+}
